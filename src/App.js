29d605374d03442e2c725edb150597f9c60f1e4f
Jhorman <jhormanfelipef@gmail.com>
2025-06-26 09:09:30 -0500

    se añade nueva logica para login2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import Login2 from './components/Login2';
 import Login from './components/Login';
 import Layout from './components/Layout';
 import './styles.css'; // Importa tu archivo CSS si lo tienes
 
 
 const App = () => {
-const [logueado, setLogueado] = useState(() => {
+  const [logueado, setLogueado] = useState(() => {
     return localStorage.getItem('logueado') === 'true';
   });
+
+  // Detecta modo oscuro desde localStorage
+  const isDarkMode = localStorage.getItem('mode') === 'dark';
 
   useEffect(() => {
     localStorage.setItem('logueado', logueado);
   }, [logueado]);
+
   return logueado ? (
     <Layout cerrarSesion={() => setLogueado(false)} />
   ) : (
-    <Login onLoginSuccess={() => setLogueado(true)} />
+    isDarkMode
+      ? <Login2 onLoginSuccess={() => setLogueado(true)} />
+      : <Login onLoginSuccess={() => setLogueado(true)} />
   );
 };
 
